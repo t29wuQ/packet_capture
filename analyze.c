bed99d645aeb43c5b3f00b0a2eab61d458463bd9
K.Tsukimoto <t.29.wuq@gmail.com>
2018-10-27 17:18:22 +0900

    add: show ip

--- analyze.c
+++ analyze.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
-#include <net/ethernet.h>
 #include <netinet/ip.h>
+#include <net/ethernet.h>
 #include "packet_capture.h"
+
+
+void analyze_ip_v4(const unsigned char *packet_pointer){
+    struct ip *header;
+    header = (struct ip *)packet_pointer;
+
+    fprintf(stdout, "Distination IP Address: ");
+    fprintf(stdout, "%s\n", inet_ntoa(header->ip_dst));
+    fprintf(stdout, "Source IP Address: ");
+    fprintf(stdout, "%s\n", inet_ntoa(header->ip_src));
+
+}
 
 void analyze_ethernet(const unsigned char *packet_pointer){
     struct ether_header *header;
@@ -16,6 +28,7 @@ void analyze_ethernet(const unsigned char *packet_pointer){
     
     switch (ntohs(header->ether_type)){
         case 0x0800: //IPv4
+            analyze_ip_v4((unsigned char *)(packet_pointer + sizeof(struct ether_header)));
             break;
         case 0x0806: //ARP
             break;
