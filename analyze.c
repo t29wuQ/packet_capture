c038ac43b18253ef34db08d3648ebe8dd1df37fb
K.Tsukimoto <t.29.wuq@gmail.com>
2018-10-28 14:11:42 +0900

    add: show tcp

--- analyze.c
+++ analyze.c
@@ -5,15 +5,30 @@
 #include <net/ethernet.h>
 #include "packet_capture.h"
 
+void analyze_tcp(const unsigned char *packet_pointer){
+    struct tcp *header;
+    header = (struct tcp *)packet_pointer;
+    fprintf(stdout, "Source Port: ");
+    fprintf(stdout, "%d\n", ntohs(header->sport));
+    fprintf(stdout, "Distination IP Address: ");
+    fprintf(stdout, "%d\n",  ntohs(header->dport));
+}
 
 void analyze_ip_v4(const unsigned char *packet_pointer){
     struct ip *header;
     header = (struct ip *)packet_pointer;
 
-    fprintf(stdout, "Distination IP Address: ");
-    fprintf(stdout, "%s\n", inet_ntoa(header->ip_dst));
     fprintf(stdout, "Source IP Address: ");
     fprintf(stdout, "%s\n", inet_ntoa(header->ip_src));
+    fprintf(stdout, "Distination IP Address: ");
+    fprintf(stdout, "%s\n", inet_ntoa(header->ip_dst));
+
+    unsigned char *next_pointer = (unsigned char *)(packet_pointer + sizeof(struct ip));
+    switch(header->ip_p){
+        case 6: //TCP
+            analyze_tcp(next_pointer);
+            break;
+    }
 
 }
 
@@ -26,9 +41,10 @@ void analyze_ethernet(const unsigned char *packet_pointer){
     fprintf(stdout, "Source Mac Address: ");
     print_mac_address(header->ether_shost);
     
+    unsigned char *next_pointer = (unsigned char *)(packet_pointer + sizeof(struct ether_header));
     switch (ntohs(header->ether_type)){
         case 0x0800: //IPv4
-            analyze_ip_v4((unsigned char *)(packet_pointer + sizeof(struct ether_header)));
+            analyze_ip_v4(next_pointer);
             break;
         case 0x0806: //ARP
             break;
--- packet_capture.h
+++ packet_capture.h
@@ -1,2 +1,12 @@
 void analyze_ethernet(const unsigned char *packet_pointer);
 void print_mac_address(u_int8_t *address);
+struct tcp{
+    u_int16_t sport;
+    u_int16_t dport;
+    u_int32_t snumber;
+    u_int32_t anumber;
+    u_int16_t flag;
+    u_int16_t wsize;
+    u_int16_t sum;
+    u_int16_t pointer;
+};
